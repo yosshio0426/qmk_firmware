78b259aaba704ce62b6e9ea6bc4abe7c869b9f5e
yosshio0426 <kuniko192002@gmail.com>
2023-05-02 09:12:33 +0900

    マウスの有効化とoverrideの有効化

--- keyboards/lily58/keymaps/yosshio0426/rules.mk
+++ keyboards/lily58/keymaps/yosshio0426/rules.mk
@@ -1,5 +1,5 @@
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no        # Mouse keys
+MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = no        # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
@@ -9,6 +9,8 @@ AUDIO_ENABLE = no           # Audio output
 RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 OLED_ENABLE= yes     # OLED display
+
+KEY_OVERRIDE_ENABLE = yes # https://qiita.com/shela/items/9e5bd102a95a15d1b00c
 
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/rgb_state_reader.c \
